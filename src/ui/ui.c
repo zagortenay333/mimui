415e9d75f0fe2bda479cd251cf8eac0f81b808d0
zagortenay333 <krionubis@gmail.com>
2026-01-31 13:05:01 +0100

    gg

--- src/ui/ui.c
+++ src/ui/ui.c
@@ -776,8 +776,6 @@ static UiRect compute_rect_intersect (UiRect r0, UiRect r1) {
 
 static Void compute_signals (UiBox *box) {
     UiSignal *sig = &box->signal;
-    Bool pressed = sig->pressed;
-    *sig = (UiSignal){};
 
     if (! (box->flags & UI_BOX_REACTIVE)) return;
 
@@ -795,7 +793,7 @@ static Void compute_signals (UiBox *box) {
 
     sig->hovered = hovered;
 
-    if (! pressed) {
+    if (! sig->pressed) {
         sig->pressed = (ui->hovered == box && (ui->event->tag == EVENT_KEY_PRESS) && (ui->event->key == GLFW_MOUSE_BUTTON_LEFT));
     } else if ((ui->event->tag == EVENT_KEY_RELEASE) && (ui->event->key == GLFW_MOUSE_BUTTON_LEFT)) {
         sig->pressed = false;
